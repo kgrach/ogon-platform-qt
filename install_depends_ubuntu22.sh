230ab4bfb0b761c1a6b60fd6247fd8816ffe3729
Kirill Grachev <pllayer@mail.ru>
2024-10-25 15:25:59 +0700

    upd

--- install_depends_ubuntu22.sh
+++ install_depends_ubuntu22.sh
@@ -1,1 +1,1 @@
-sudo apt install libxkbcommon-dev libfontconfig1-dev libmtdev-dev libudev-dev libegl1-mesa-dev qt5-qmake qtbase5-private-dev
+sudo apt install -y libxkbcommon-dev libfontconfig1-dev libmtdev-dev libudev-dev libegl1-mesa-dev qt5-qmake qtbase5-private-dev
