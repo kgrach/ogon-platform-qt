dffc77b53f3e59962ad0149f1b442387f3bde61e
Kirill Grachev <pllayer@mail.ru>
2024-10-30 16:00:11 +0700

    Upd 4 RedOs

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-qtchooser -qt=qt5 -run-tool=qmake ADDITIONAL_RPATHS=/opt/ogon/lib/:/opt/ogon/lib/x86_64-linux-gnu/pkgconfig/ PREFIX=/opt/ogon
+/usr/lib/qtchooser/qtchooser -qt=qt5 -run-tool=qmake ADDITIONAL_RPATHS=/opt/ogon/lib/:/opt/ogon/lib/x86_64-linux-gnu/pkgconfig/ PREFIX=/opt/ogon
 export PKG_CONFIG_PATH=/opt/ogon/lib/pkgconfig/:/opt/ogon/lib/x86_64-linux-gnu/pkgconfig/
 make install
